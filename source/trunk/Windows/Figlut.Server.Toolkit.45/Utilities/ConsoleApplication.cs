d099ab83dc5dd7ef523b7be01c045219b0ae43d8
Paul Kolozsvari <paulkolo@figlut.com>
2017-12-03 14:36:06 +0200

    Added a ConsoleApplication class for functions related to the Console apps. This class now has a function for maximizing the console's command prompt.

---
+++ source/trunk/Windows/Figlut.Server.Toolkit.45/Utilities/ConsoleApplication.cs
@@ -0,0 +1,26 @@
+﻿namespace Figlut.Server.Toolkit.Utilities
+{
+    #region Using Directives
+
+    using System.Diagnostics;
+    using System.Runtime.InteropServices;
+
+    #endregion //Using Directives
+
+    public class ConsoleApplication
+    {
+        #region Methods
+
+
+        [DllImport("user32.dll")]
+        public static extern bool ShowWindow(System.IntPtr hWnd, int cmdShow);
+
+        public static void Maximize()
+        {
+            Process p = Process.GetCurrentProcess();
+            ShowWindow(p.MainWindowHandle, 3); //SW_MAXIMIZE = 3
+        }
+
+        #endregion //Methods
+    }
+}
