896df17e583cb14c1667bd3b56a85d441dd318db
Paul Kolozsvari <paulkolo@figlut.com>
2018-05-06 21:07:21 +0200

    Added iCal class for downloading an .ics calendar file for public holidays.

---
+++ source/trunk/Windows/Figlut.Server.Toolkit.45/Data/iCalendar/ICalCalendarDownloader.cs
@@ -0,0 +1,108 @@
+﻿namespace Figlut.Server.Toolkit.Data.iCalendar
+{
+    using Figlut.Server.Toolkit.Data;
+    #region Using Directives
+
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Linq;
+    using System.Net;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    #endregion //Using Directives
+
+    /// <summary>
+    /// Utility class that downloads an .ics file from a website and converts to to an ICalCalendar object.
+    /// </summary>
+    public class ICalCalendarDownloader
+    {
+        #region Constructors
+
+        /// <summary>
+        /// Provide a URL which contains parameter place holders for the country code, start date and end date.
+        /// e.g. a URL such as this http://www.kayaposoft.com/enrico/ics/v1.0?country=zaf&fromDate=01-01-2018&toDate=31-12-2018
+        /// would be specified in this constructor as http://www.kayaposoft.com/enrico/ics/v1.0?country={0}&fromDate={1}&toDate={2}
+        /// in order to allow for the replacement of the country code {0}, start date {1} and end date {2}.
+        /// </summary>
+        /// <param name="downloadUrl"></param>
+        public ICalCalendarDownloader(string downloadUrl)
+        {
+            DownloadUrl = downloadUrl;
+        }
+
+        #endregion //Constructors
+
+        #region Fields
+
+        private string _downloadUrl;
+
+        #endregion //Fields
+
+        #region Properties
+
+        /// <summary>
+        ///  /// Provide a URL which contains parameter place holders for the country code, start date and end date.
+        /// e.g. a URL such as this http://www.kayaposoft.com/enrico/ics/v1.0?country=zaf&fromDate=01-01-2018&toDate=31-12-2018
+        /// would be specified in this constructor as http://www.kayaposoft.com/enrico/ics/v1.0?country={0}&fromDate={1}&toDate={2}
+        /// in order to allow for the replacement of the country code {0}, start date {1} and end date {2}.
+        /// </summary>
+        public string DownloadUrl
+        {
+            get { return _downloadUrl; }
+            set
+            {
+                if (string.IsNullOrEmpty(value))
+                {
+                    throw new NullReferenceException(string.Format("{0} may not be null or empty when constructing a {1}.",
+                        EntityReader<ICalCalendarDownloader>.GetPropertyName(p => p.DownloadUrl, false),
+                        typeof(ICalCalendarDownloader).Name));
+                }
+                _downloadUrl = value;
+            }
+        }
+
+        #endregion //Properties
+
+        #region Methods
+
+        /// <summary>
+        /// The country code, start date and end date will be applied to the download URL of this ICalCalendarDownloader.
+        /// The Country Name is only used for display purposes when constructing the resulting the ICalCalendar.
+        /// If the output file name is not specified, a temp file path will be generated in the current user's temp folder.
+        /// </summary>
+        /// <returns></returns>
+        public ICalCalendar DownloadICalCalendar(
+            string countryCode, 
+            string countryName, 
+            string startDate, 
+            string endDate,
+            string outputFilePath,
+            bool deleteOutputFileAfterParsing)
+        {
+            if (string.IsNullOrEmpty(outputFilePath))
+            {
+                string outputFileName = string.Format("{0}-{1}-{2}{3}", countryCode, startDate, endDate, ICalPublicHolidayParser.ICALENDAR_FILE_EXTENSION);
+                outputFilePath = Path.Combine(Path.GetTempPath(), outputFileName);
+            }
+            using (WebClient webClient = new WebClient())
+            {
+                string formattedDownloadUrl = string.Format(_downloadUrl, countryCode, startDate, endDate);
+                if (File.Exists(outputFilePath))
+                {
+                    File.Delete(outputFilePath);
+                }
+                webClient.DownloadFile(formattedDownloadUrl, outputFilePath);
+            }
+            ICalCalendar result = ICalPublicHolidayParser.ParseICalendarFile(outputFilePath, countryCode, countryName);
+            if (deleteOutputFileAfterParsing)
+            {
+                File.Delete(outputFilePath);
+            }
+            return result;
+        }
+
+        #endregion //Methods
+    }
+}
