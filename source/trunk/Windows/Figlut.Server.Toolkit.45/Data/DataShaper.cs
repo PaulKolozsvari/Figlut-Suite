0cec55ab50190572b82fa54b11534526360a0cbf
Paul Kolozsvari <paulkolo@figlut.com>
2018-05-15 21:08:27 +0200

    Changed the Email validation method to use the MVS annotations assembly to use the default .NET email validation code.

--- source/trunk/Windows/Figlut.Server.Toolkit.45/Data/DataShaper.cs
+++ source/trunk/Windows/Figlut.Server.Toolkit.45/Data/DataShaper.cs
@@ -4,6 +4,7 @@
 
     using System;
     using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
     using System.Globalization;
     using System.Linq;
     using System.Text;
@@ -73,40 +74,7 @@
 
         public static bool IsValidEmail(string emailAddress)
         {
-            invalid = false;
-            if (string.IsNullOrEmpty(emailAddress))
-            {
-                return false;
-            }
-            // Use IdnMapping class to convert Unicode domain names. 
-            try
-            {
-                emailAddress = Regex.Replace(
-                    emailAddress, @"(@)(.+)$", 
-                    DomainMapper,
-                    RegexOptions.None, 
-                    TimeSpan.FromMilliseconds(200));
-            }
-            catch (RegexMatchTimeoutException)
-            {
-                return false;
-            }
-            if (invalid)
-            {
-                return false;
-            }
-            // Return true if strIn is in valid e-mail format. 
-            try
-            {
-                return Regex.IsMatch(emailAddress,
-                      @"^(?("")("".+?(?<!\\)""@)|(([0-9a-z]((\.(?!\.))|[-!#\$%&'\*\+/=\?\^`\{\}\|~\w])*)(?<=[0-9a-z])@))" +
-                      @"(?(\[)(\[(\d{1,3}\.){3}\d{1,3}\])|(([0-9a-z][-\w]*[0-9a-z]*\.)+[a-z0-9][\-a-z0-9]{0,22}[a-z0-9]))$",
-                      RegexOptions.IgnoreCase, TimeSpan.FromMilliseconds(250));
-            }
-            catch (RegexMatchTimeoutException)
-            {
-                return false;
-            }
+            return (new EmailAddressAttribute().IsValid(emailAddress));
         }
 
         private static string DomainMapper(Match match)
