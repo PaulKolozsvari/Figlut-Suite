cdf5a930dfe6d936b4e3d1c3e672722a767cd5ef
Paul Kolozsvari <paulkolo@figlut.com>
2018-05-15 18:32:24 +0200

    Added phone number validation class to the server toolkit.

--- source/trunk/Windows/Figlut.Server.Toolkit.45/Data/DataShaper.cs
+++ source/trunk/Windows/Figlut.Server.Toolkit.45/Data/DataShaper.cs
@@ -66,18 +66,23 @@
 
         private static bool invalid = false;
 
-        public static bool IsValidEmail(string strIn)
+        public static bool IsValidPhoneNumber(string number)
+        {
+            return new PhoneNumberValidator().IsValidPhoneNumber(ref number);
+        }
+
+        public static bool IsValidEmail(string emailAddress)
         {
             invalid = false;
-            if (String.IsNullOrEmpty(strIn))
+            if (string.IsNullOrEmpty(emailAddress))
             {
                 return false;
             }
             // Use IdnMapping class to convert Unicode domain names. 
             try
             {
-                strIn = Regex.Replace(
-                    strIn, @"(@)(.+)$", 
+                emailAddress = Regex.Replace(
+                    emailAddress, @"(@)(.+)$", 
                     DomainMapper,
                     RegexOptions.None, 
                     TimeSpan.FromMilliseconds(200));
@@ -93,7 +98,7 @@
             // Return true if strIn is in valid e-mail format. 
             try
             {
-                return Regex.IsMatch(strIn,
+                return Regex.IsMatch(emailAddress,
                       @"^(?("")("".+?(?<!\\)""@)|(([0-9a-z]((\.(?!\.))|[-!#\$%&'\*\+/=\?\^`\{\}\|~\w])*)(?<=[0-9a-z])@))" +
                       @"(?(\[)(\[(\d{1,3}\.){3}\d{1,3}\])|(([0-9a-z][-\w]*[0-9a-z]*\.)+[a-z0-9][\-a-z0-9]{0,22}[a-z0-9]))$",
                       RegexOptions.IgnoreCase, TimeSpan.FromMilliseconds(250));
